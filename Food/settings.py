b8f4076c788b2e7bbba74bcdcc22f808bd49d0b6
sondos-medhat <56825620+sondos-medhat@users.noreply.github.com>
2021-04-16 00:57:35 +0200

    Update settings.py

--- Food/settings.py
+++ Food/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'm9(ir1@*#h1xe1+r#b%-ml-if-+11bez7il&f)9@mn7@r1azoe'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
